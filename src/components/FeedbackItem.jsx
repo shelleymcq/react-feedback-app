acebdb5815e32246a925314ea6f2d01492ebde0d
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-12-30 13:42:49 -0500

    added components for feedback rating and stats

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,17 @@
+
+import { FaTimes } from 'react-icons/fa'   // import the FaTimes cion from the font awesome library
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({item}) {
+function FeedbackItem({ item, handleDelete }) {
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        {/* FaTimes takes in a color */}
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,23 +1,27 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem'
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({feedback}) {
-  if(!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet</p>
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
   }
 
   return (
     <div className="feedback=list">
       {feedback.map((item) => (
         // display feedback item
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
-  )
+  );
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.array.isRequired,
-}
+};
 
-export default FeedbackList
+export default FeedbackList;
