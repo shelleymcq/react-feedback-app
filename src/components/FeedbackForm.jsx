38d5b757891c072c4ac564149363581fa8ab5cb2
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2022-01-03 17:01:17 -0500

    added addFeedback, but broke average

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,9 +3,9 @@ import RatingSelect from "./RatingSelect"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState('')
-  const [rating, setRating] = useState(10)
+  const [rating, setRating] = useState()
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
@@ -24,11 +24,24 @@ function FeedbackForm() {
     setText(event.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(text.trim().length >= 10) {
+      const newFeedback = {
+        text,
+        rating
+      }
+
+      handleAdd(newFeedback)
+      setText('')
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service today?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
           <Button type="submit" isDisabled={btnDisabled}>
