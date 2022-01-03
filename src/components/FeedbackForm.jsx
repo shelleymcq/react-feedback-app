092c79f31564de2e09508a1256fa0894566ede30
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2022-01-03 13:55:04 -0500

    add components for buttons and feedback form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import RatingSelect from "./RatingSelect"
+import Card from "./shared/Card"
+import Button from "./shared/Button"
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const handleTextChange = (event) => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length < 10) {
+      setBtnDisabled(true)
+      setMessage("Text must be at least 10 characters")
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+
+    setText(event.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service today?</h2>
+        <RatingSelect />
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function RatingSelect() {
+  const [selected, setSelected] = useState(10)
+
+  return (
+    <div>
+      Rating Select
+    </div>
+  )
+}
+
+export default RatingSelect
