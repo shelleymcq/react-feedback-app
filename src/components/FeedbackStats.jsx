7804965f61f4c1dc619750942f6dcdeaaf31ae6d
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2022-01-06 10:58:44 -0500

    fix: average calculation

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -5,7 +5,8 @@ function FeedbackStats() {
   const {feedback} = useContext(FeedbackContext)
   // calculate ratings average
   let average = feedback.reduce((accumulator, current) => {
-    return accumulator + current.rating
+    // convert rating from a string to a number with unary operator (+)
+    return accumulator + +current.rating
   }, 0) / feedback.length
 
   average = average.toFixed(1).replace(/[..]0$/, '')
